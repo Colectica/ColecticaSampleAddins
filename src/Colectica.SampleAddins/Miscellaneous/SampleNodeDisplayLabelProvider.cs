8b9ff32b9df49bf3095a86f400aab9a3cbf40d6b
Jeremy Iverson <jeremy@colectica.com>
2015-09-14 15:36:48 -0500

    Sample INodeDisplayLabelProvider addin

---
+++ src/Colectica.SampleAddins/Miscellaneous/SampleNodeDisplayLabelProvider.cs
@@ -0,0 +1,19 @@
+﻿using Algenta.Colectica.ViewModel.Navigator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colectica.SampleAddins.Miscellaneous
+{
+    [Export(typeof(INodeDisplayLabelProvider))]
+    public class SampleNodeDisplayLabelProvider : INodeDisplayLabelProvider
+    {
+        public string GetDisplayLabelForItem(Algenta.Colectica.Model.Repository.RepositoryItemMetadata item)
+        {
+            return item.Label.Best;
+        }
+    }
+}
