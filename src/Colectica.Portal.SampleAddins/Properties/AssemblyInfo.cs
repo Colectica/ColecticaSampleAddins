d4b3a1c8a38f2c65990ed18e8ccbb4ca4efd3523
Jeremy Iverson <jeremy@colectica.com>
2016-04-26 15:10:15 -0500

    Sample Portal view addin

---
+++ src/Colectica.Portal.SampleAddins/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Colectica.Portal.SampleAddins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Colectica.Portal.SampleAddins")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eafc81b7-8d69-49fe-8563-657f27c8b295")]
---
+++ src/Colectica.Portal.SampleAddins/ViewInformation/SampleAddinViewInformation.cs
@@ -0,0 +1,37 @@
+﻿using Colectica.Portal.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Algenta.Colectica.Model;
+using Algenta.Colectica.Model.Ddi;
+using Colectica.Common.ItemViewModels;
+
+namespace Colectica.Portal.SampleAddins.ViewInformation
+{
+    [Export(typeof(IPortalViewInformation))]
+    public class SampleAddinViewInformation : IPortalViewInformation
+    {
+        public Guid ItemType
+        {
+            get
+            {
+                return DdiItemType.OrganizationScheme;
+            }
+        }
+
+        public string ViewLocation
+        {
+            get
+            {
+                return "/Views/SampleView.cshtml";
+            }
+        }
+
+        public VersionableItemViewModelBase GetViewModelForItem(IVersionable item)
+        {
+            return new VersionableItemViewModelBase<OrganizationScheme>(item as OrganizationScheme);
+        }
+    }
+}
