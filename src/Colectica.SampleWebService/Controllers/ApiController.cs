e34771ad366a365fefe2740ba30e0814ea169792
Jeremy Iverson <jeremy@colectica.com>
2019-02-12 08:37:16 -0600

    Sample web service project to receive calls from item page and basket page buttons in Colectica Portal

---
+++ src/Colectica.SampleWebService/Controllers/ApiController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colectica.SampleWebService.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colectica.SampleWebService.Controllers
+{
+    [Route("")]
+    public class ApiController : ControllerBase
+    {
+        [HttpPost("item-js-handler")]
+        public ActionResult<ApiResponse> ItemHandler([FromBody]ItemRequest request)
+        {
+            var id = request.ItemIdentifier;
+
+            var response = new ApiResponse();
+            response.Message = $"I received the following identification information: {id}";
+            return response;
+        }
+
+        [HttpPost("item-js-handler-redirect")]
+        public ActionResult<ApiResponse> BasketHandlerWithRedirect([FromBody]ItemRequest request)
+        {
+            var response = new ApiResponse();
+            response.RedirectUrl = "http://example.org";
+            return response;
+        }
+
+
+        [HttpPost("basket-js-handler")]
+        public ActionResult<ApiResponse> BasketHandlerWithMessage([FromBody]BasketRequest request)
+        {
+            var response = new ApiResponse();
+            response.Message = $"I received information about a basket named {request.Citation.Title.EnglishUS}. It has {request.ItemIdentifers.Length} items in it.";
+            return response;
+        }
+
+        [HttpPost("basket-js-handler-redirect")]
+        public ActionResult<ApiResponse> BasketHandlerWithRedirect([FromBody] BasketRequest request)
+        {
+            var response = new ApiResponse();
+            response.RedirectUrl = "http://example.org";
+            return response;
+        }
+
+    }
+
+}
---
+++ src/Colectica.SampleWebService/Data.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colectica.SampleWebService.Data
+{
+    public class ItemRequest
+    {
+        public string ItemIdentifier { get; set; }
+    }
+
+    public partial class BasketRequest
+    {
+        [JsonProperty("Citation", NullValueHandling = NullValueHandling.Ignore)]
+        public Citation Citation { get; set; }
+
+        [JsonProperty("ItemIdentifers", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] ItemIdentifers { get; set; }
+    }
+
+    public partial class Citation
+    {
+        [JsonProperty("Title")]
+        public Title Title { get; set; }
+    }
+
+    public partial class Title
+    {
+        [JsonProperty("en-US")]
+        public string EnglishUS { get; set; }
+    }
+
+    public class ApiResponse
+    {
+        public string Message { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+
+}
